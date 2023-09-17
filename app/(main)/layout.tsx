3f85ad24567d86caa22ca01ef7bce359f4fe7d8f
Sahil Shah <shahsahil713@gmail.com>
2023-09-17 18:50:21 +0530

    ui optimizations

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,9 @@
 "use client"
+import { Separator } from "@/components/ui/separator"
 import Sidebar, { SidebarItem } from "@/components/ui/sidebar"
 import SiteNav from "@/components/ui/site-nav"
 import { RootState } from "@/store"
-import { LayoutDashboard } from "lucide-react"
+import { BarChart3, Boxes, LayoutDashboard, LifeBuoy, Package, Receipt, Settings, UserCircle } from "lucide-react"
 import { useSelector } from "react-redux"
 
 interface MainLayoutProps {
@@ -15,8 +16,44 @@ export default function MainLayout({
   return (
     <div>
       <SiteNav />
-      <Sidebar>
-        <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" alert />
+      <Sidebar className="pt-2" >
+        <SidebarItem item={{
+          icon: <LayoutDashboard size={20} />,
+          title: "Statistics",
+          href: '/statistics'
+        }} />
+        <SidebarItem item={{
+          icon: <BarChart3 size={20} />,
+          title: "Users",
+          href: '/Users'
+        }} />
+        <SidebarItem item={{
+          icon: <UserCircle size={20} />,
+          title: "Inventory",
+          href: '/Inventory'
+        }} />
+        <Separator />
+        <SidebarItem item={{
+          icon: <Boxes size={20} />,
+          title: "Orders",
+          href: '/Orders'
+        }} />
+        <SidebarItem item={{
+          icon: <Package size={20} />,
+          title: "Billings",
+          href: '/Billings'
+        }} />
+        <SidebarItem item={{
+          icon: <Receipt size={20} />,
+          title: "Settings",
+          href: '/Settings'
+        }} />
+        <Separator />
+        <SidebarItem item={{
+          icon: <Settings size={20} />,
+          title: "Help",
+          href: '/Help'
+        }} />
       </Sidebar>
       <div className={`${isOpen ? 'pl-60' : 'pl-16'} transition-all`}>
         {children}
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -1,8 +1,13 @@
 "use client"
-import React, { useContext, createContext, useState, ReactNode } from "react";
+import React, { useContext, createContext, ReactNode } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useSelector } from "react-redux";
 import { RootState, toggleSidebar, useAppDispatch } from "@/store";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./button";
+import { usePathname } from "next/navigation";
+import { ActionTooltip } from "./action-tooltip";
 
 interface SidebarContextType {
     isOpen: boolean;
@@ -10,23 +15,29 @@ interface SidebarContextType {
 
 const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
 
-interface SidebarProps {
+interface SidebarProps extends React.HTMLAttributes<HTMLElement> {
     children: ReactNode;
 }
 
-export default function Sidebar({ children }: SidebarProps): JSX.Element {
-    const { isOpen } = useSelector((state: RootState) => state.ui.sidebar)
+export default function Sidebar({
+    children,
+    className,
+}: SidebarProps): JSX.Element {
+    const { isOpen } = useSelector((state: RootState) => state.ui.sidebar);
     const dispatch = useAppDispatch();
+
+    const sidebarClasses = `h-full flex flex-col border-r shadow-sm relative ${className}`;
+
     return (
         <div className="fixed z-20">
             <aside className="h-screen">
-                <nav className="h-full flex flex-col  border-r shadow-sm relative">
+                <nav className={sidebarClasses}>
                     <SidebarContext.Provider value={{ isOpen }}>
-                        <ul className="flex-1 px-3">{children}</ul>
+                        <ul className="flex-1 px-3 gap-3 flex flex-col">{children}</ul>
                     </SidebarContext.Provider>
                     <button
                         onClick={() => dispatch(toggleSidebar())}
-                        className="p-0.5 border bg-background absolute top-1 -right-3.5 rounded-full"
+                        className="p-0.5 border bg-background absolute top-4 -right-3.5 rounded-full"
                     >
                         {isOpen ? <ChevronLeft size={20} /> : <ChevronRight size={20} />}
                     </button>
@@ -35,49 +46,42 @@ export default function Sidebar({ children }: SidebarProps): JSX.Element {
         </div>
     );
 }
-interface SidebarItemProps {
-    icon: JSX.Element;
-    text: string;
-    active?: boolean;
-    alert?: boolean;
+
+interface SidebarItemProps extends React.HTMLAttributes<HTMLElement> {
+    item: {
+        href: string
+        title: string
+        icon: JSX.Element;
+    }
 }
 
-export function SidebarItem({ icon, text, active, alert }: SidebarItemProps): JSX.Element {
+export function SidebarItem({ item }: SidebarItemProps): JSX.Element {
     const { isOpen } = useContext(SidebarContext) || { isOpen: true };
+    const pathname = usePathname();
+
+    const linkClasses = cn(
+        buttonVariants({ variant: "ghost" }),
+        pathname === item.href ? "bg-muted hover:bg-muted" : "",
+        "justify-start relative transition-colors group"
+    );
+
+    const linkContent = (
+        <Link href={item.href} className={linkClasses}>
+            {item.icon}
+            <span className={`overflow-hidden transition-all ${isOpen ? "w-40 ml-3" : "w-0"}`}>
+                {item.title}
+            </span>
+        </Link>
+    );
 
     return (
-        <li
-            className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group
-        ${active
-                    ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
-                    : "hover:bg-indigo-50 text-gray-600"
-                }
-    `}
-        >
-            {icon}
-            <span
-                className={`overflow-hidden transition-all ${isOpen ? "w-40 ml-3" : "w-0"}`}
-            >
-                {text}
-            </span>
-            {alert && (
-                <div
-                    className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${isOpen ? "" : "top-2"
-                        }`}
-                />
-            )}
-
-            {!isOpen && (
-                <div
-                    className={`
-          absolute left-full rounded-md px-2 py-1 ml-6
-          bg-indigo-100 text-indigo-800 text-sm
-          invisible opacity-20 -translate-x-3 transition-all
-          group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
-      `}
-                >
-                    {text}
-                </div>
+        <li>
+            {!isOpen ? (
+                <ActionTooltip side="right" align="center" label={item.title}>
+                    {linkContent}
+                </ActionTooltip>
+            ) : (
+                linkContent
             )}
         </li>
     );
