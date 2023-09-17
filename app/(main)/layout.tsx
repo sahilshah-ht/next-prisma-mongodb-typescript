fa14e8700811af2ced769b009ebfb6e8d6360ced
Sahil Shah <shahsahil713@gmail.com>
2023-09-17 09:59:31 +0530

    common header added

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,9 @@
+import { MainNav } from "@/components/ui/main-nav"
+import { ModeToggle } from "@/components/ui/mode-toggle"
+import { Search } from "@/components/ui/search"
+import TeamSwitcher from "@/components/ui/team-switcher"
+import { UserNav } from "@/components/ui/user-nav"
+
 interface MainLayoutProps {
   children: React.ReactNode
 }
@@ -7,6 +13,19 @@ export default function MainLayout({
 
   return (
     <div>
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <TeamSwitcher />
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search />
+              <ModeToggle />
+              <UserNav />
+            </div>
+          </div>
+        </div>
+      </div>
       {children}
     </div>
   )
